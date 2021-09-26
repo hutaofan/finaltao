75d072871a8cf277d316555eaf7760efe41656df
hutaofan <90136089+hutaofan@users.noreply.github.com>
2021-09-26 14:27:45 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+﻿// console.log('Hi!');
+function changeVideo(url){
+    document.getElementById('video-slider').src = url;
+}
+
+//вкладка смена информации при потере активности
+
+let timer;
+let t = document.querySelector('head title').innerHTML;
+
+function changeTitle(icon, text) {
+    document.querySelector('head title').innerHTML = text;
+    document.querySelector('link[rel="icon"]').setAttribute('href', icon);
+}
+
+window.onblur = () => {
+    timer = setTimeout(() => {
+        changeTitle('./heart.ico', 'Вернитесь...🙀🤥😪');
+    }, 3000)
+}
+
+window.onfocus = () => {
+    clearTimeout(timer);
+    changeTitle('./favicon.ico', t);
+}
